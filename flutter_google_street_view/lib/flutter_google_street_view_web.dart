74259c59e5ad002d91dc90eb32d44d5fc0e9aa1e
Alexis Glass <alexis.glass@scandit.com>
2024-07-18 21:42:23 +0200

    Minor tweaks for latest dart

--- flutter_google_street_view/lib/flutter_google_street_view_web.dart
+++ flutter_google_street_view/lib/flutter_google_street_view_web.dart
@@ -12,5 +12,6 @@ import 'package:google_maps/google_maps.dart' as gmaps;
 import 'package:google_maps/google_maps.dart';
 import 'package:kotlin_scope_function/kotlin_scope_function.dart';
 import 'package:flutter_google_street_view/src/web/shims/dart_ui.dart' as ui;
+import 'dart:ui_web' as uiWeb;
 
 part 'package:flutter_google_street_view/src/web/plugin.dart';
